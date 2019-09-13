01effb2b058291653142efe46bdcfa4ac4cf1362
Mohamed Taher <mohmdtaher96@gmail.com>
2019-09-13 15:40:08 -0700

    first commit

---
+++ customSlider/CustomSlider.swift
@@ -0,0 +1,21 @@
+//
+//  CustomSlider.swift
+//  customSlider
+//
+//  Created by user on 8/22/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class CustomSlider: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
